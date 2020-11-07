8494a6ccc05584d69ddde287a9c318b2cdd23ca0
Gideon Cooper <Gideon.Cooper@hotmail.com>
2020-11-07 16:55:37 +1300

    set up nav and starting to set up home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
 
+const styles = {
+  container: {
+    backgroundColor: 'red',
+    height: '80vh',
+  },
+}
 export default function Home() {
-    return (
-       <Container>
-           
-       </Container>
-    )
+  return <Container style={styles.container}></Container>
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,29 +2,32 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import FormControl from 'react-bootstrap/FormControl'
+
+const styles = {
+  nav: {
+    backgroundColor: 'white',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  navRight: {},
+  brand: {
+    color: '#E51937',
+    fontSize: 28,
+    letterSpacing: 4,
+  },
+}
 
 export default function Navigation() {
   return (
-    <Navbar bg='light' expand='lg'>
-      <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls='basic-navbar-nav' />
-      <Navbar.Collapse id='basic-navbar-nav'>
-        <Nav className='mr-auto'>
-          <Nav.Link href='#home'>Home</Nav.Link>
-          <Nav.Link href='#link'>Link</Nav.Link>
-          <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-            <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-            <NavDropdown.Item href='#action/3.2'>
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href='#action/3.4'>
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
+    <Navbar style={styles.nav} expand='lg'>
+      <Navbar.Brand style={styles.brand}>Linkup</Navbar.Brand>
+      <Nav style={styles.navRight}>
+        <Nav.Link>Login</Nav.Link>
+        <Nav.Link>Register</Nav.Link>
+      </Nav>
     </Navbar>
   )
 }
