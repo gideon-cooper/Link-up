a34f5b62d220281d2ef0dabdfcc3587c45bbd7b5
Gideon Cooper <Gideon.Cooper@hotmail.com>
2020-11-07 14:40:11 +1300

    added react bootstrap and set up initial dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.scss';
-
+import React from 'react'
+import { Route } from 'react-router-dom'
+import './App.css'
+import Home from './components/Home'
+import Navigation from './components/Navigation'
 function App() {
   return (
-    <div className="App">
-    
+    <div className='App'>
+      <Navigation />
+      <Route path='/' component={Home} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
-);
-
+)
